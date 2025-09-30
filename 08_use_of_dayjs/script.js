c674b4b9ee8f4a8b26a52c93750537c7e697ab7e
bear743 <1181472525@qq.com>
2025-09-30 10:38:06 +0800

    added timezone editing feature (unfinished)

--- 08_use_of_dayjs/script.js
+++ 08_use_of_dayjs/script.js
@@ -1,12 +1,60 @@
 function updateDateTime(){
-    const now = dayjs()
+    const currentTimezone = localStorage.getItem('selectedTimezone') || dayjs.tz.guess()
+
+    const now = dayjs().tz(currentTimezone)
 
     const time = now.format('HH:mm:ss')
     const date = now.format('dddd, MMMM D, YYYY')
 
     document.getElementById('time').innerText = time
     document.getElementById('date').innerText = date
+
+    document.getElementById('timezone').innerText = currentTimezone.replace(/_/g, ' ').split('/').join(' / ')
+}
+
+function populateTimezones(){
+    const timezoneSelect = document.getElementById('timezones')
+    const timezones = Intl.supportedValuesOf('timeZone')
+
+    timezoneSelect.innerHTML = '';
+
+    const currentTimezone = localStorage.getItem('selectedTimezone') || dayjs.tz.guess()
+
+    timezones.forEach(tz => {
+        const option = document.createElement('option')
+        option.value = tz
+        option.innerText = tz
+        if (tz === currentTimezone) {
+            option.selected = true
+        }
+        timezoneSelect.appendChild(option)
+    })
+}
+
+function setupTimezoneChange(){
+    const timezoneSelect = document.getElementById('timezones')
+    const applyButton = document.querySelector('.apply-btn')
+    const getTzButton = document.querySelector('get-tz-btn')
+
+    applyButton.addEventListener('click', () => {
+        const selectedTimezone = timezoneSelect.value
+
+        localStorage.setItem('selectedTimezone', selectedTimezone)
+
+        updateDateTime()
+    })
+
+    getTzButton.addEventListener('click', () => {
+        
+    })
 }
 
 updateDateTime()
 setInterval(updateDateTime, 1000)
+
+document.addEventListener('DOMContentLoaded', () => {
+    MicroModal.init()
+    populateTimezones()
+    setupTimezoneChange()
+})
+
